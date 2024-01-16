57865c354fb9cc3e5064e05bb70317be62bf23d6
Botirbek2702 <userbotirbek2702@gmail.com>
2024-01-16 23:13:33 +0500

    Expressjs

---
+++ index.js
@@ -0,0 +1,30 @@
+import express from "express"
+import { create, engine } from 'express-handlebars';
+const app = express()
+
+const hbs =create({defaultLayout: "main" , extname: "hbs"})
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', './views');
+
+app.get('/' , (req,res) =>{
+    res.render("index")
+})
+app.get('/about' , (req,res) =>{
+    res.render("about")
+})
+
+
+
+
+
+
+
+
+
+
+const PORT =process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
