b0da1d2913bc643551cb0192ca958a4da92833ec
Botirbek2702 <userbotirbek2702@gmail.com>
2024-01-26 22:42:53 +0500

    new

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
 import express from "express"
+import mongoose from "mongoose"
 import { create, engine } from 'express-handlebars';
+import * as dotenv from "dotenv"
+import auth from "./views/routes/auth.js"
+import product from "./views/routes/product.js"
+dotenv.config()
 const app = express()
-
 const hbs =create({defaultLayout: "main" , extname: "hbs"})
 
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
-app.set('views', './views');
+app.set('views', './views');app.use(auth)
+app.use(product)
 app.use(express.static("public"))
 app.use(express.urlencoded({extended:true}))
-import auth from "./views/routes/auth.js"
-import product from "./views/routes/product.js"
-
-app.use(auth)
-app.use(product)
+app.use(express.json())
 
 
 
 
 
-//1GN32zKYNIeLQLeL  
-//mongodb+srv://userbotirbek2702:<password>@cluster0.h3tbroj.mongodb.net/?retryWrites=true&w=majority
+
+mongoose.set("strictQuery" , false)
+mongoose.connect(process.env.Mongo_Uri, () => console.log("Mongodb conected"))
+
 const PORT =process.env.PORT || 3000
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
