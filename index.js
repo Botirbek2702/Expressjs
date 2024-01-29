e78f3d891f8e03cec3ab72526ae6ef6c5c4c916e
Botirbek2702 <userbotirbek2702@gmail.com>
2024-01-29 19:05:13 +0500

    new code

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
 import express from "express"
-import mongoose from "mongoose"
 import { create, engine } from 'express-handlebars';
 import * as dotenv from "dotenv"
 import auth from "./views/routes/auth.js"
 import product from "./views/routes/product.js"
+import mongoose from "mongoose";
 dotenv.config()
+
 const app = express()
+
 const hbs =create({defaultLayout: "main" , extname: "hbs"})
 
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
-app.set('views', './views');app.use(auth)
-app.use(product)
+app.set('views', './views');
+
 app.use(express.static("public"))
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 
 
 
+app.use(auth)
+app.use(product)
 
 
 
-mongoose.set("strictQuery" , false)
-mongoose.connect(process.env.Mongo_Uri,{ useNewUrlParser: true }, () => console.log("Mongodb conected"))
 
 const PORT =process.env.PORT || 3000
 app.listen(PORT, () => {
